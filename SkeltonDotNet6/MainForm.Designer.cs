4b86e113594f84d3353156f920b0a9b5d877d9db
古橋 宏 <bryful@gmail.com>
2023-08-24 18:55:30 +0900

    up

--- SkeltonDotNet6/MainForm.Designer.cs
+++ SkeltonDotNet6/MainForm.Designer.cs
@@ -29,6 +29,10 @@
 		private void InitializeComponent()
 		{
 			textBox1 = new TextBox();
+			menuStrip1 = new MenuStrip();
+			fileToolStripMenuItem = new ToolStripMenuItem();
+			quitToolStripMenuItem = new ToolStripMenuItem();
+			menuStrip1.SuspendLayout();
 			SuspendLayout();
 			// 
 			// textBox1
@@ -37,21 +41,54 @@
 			textBox1.BackColor = Color.FromArgb(64, 64, 64);
 			textBox1.BorderStyle = BorderStyle.FixedSingle;
 			textBox1.ForeColor = Color.FromArgb(230, 230, 230);
-			textBox1.Location = new Point(12, 47);
+			textBox1.Location = new Point(12, 62);
 			textBox1.Multiline = true;
 			textBox1.Name = "textBox1";
-			textBox1.Size = new Size(863, 374);
+			textBox1.Size = new Size(642, 309);
 			textBox1.TabIndex = 0;
+			// 
+			// menuStrip1
+			// 
+			menuStrip1.Anchor = AnchorStyles.None;
+			menuStrip1.AutoSize = false;
+			menuStrip1.BackColor = SystemColors.Control;
+			menuStrip1.Dock = DockStyle.None;
+			menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+			menuStrip1.Location = new Point(0, 22);
+			menuStrip1.Name = "menuStrip1";
+			menuStrip1.Size = new Size(675, 24);
+			menuStrip1.TabIndex = 1;
+			menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			fileToolStripMenuItem.BackColor = SystemColors.Control;
+			fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { quitToolStripMenuItem });
+			fileToolStripMenuItem.ForeColor = SystemColors.ControlText;
+			fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			fileToolStripMenuItem.Size = new Size(37, 20);
+			fileToolStripMenuItem.Text = "File";
+			// 
+			// quitToolStripMenuItem
+			// 
+			quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+			quitToolStripMenuItem.Size = new Size(180, 22);
+			quitToolStripMenuItem.Text = "Quit";
 			// 
 			// MainForm
 			// 
 			AllowDrop = true;
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(896, 442);
+			BackColor = Color.FromArgb(64, 64, 64);
+			ClientSize = new Size(675, 392);
 			Controls.Add(textBox1);
+			Controls.Add(menuStrip1);
+			MainMenuStrip = menuStrip1;
 			Name = "MainForm";
 			Text = "MainForm";
+			menuStrip1.ResumeLayout(false);
+			menuStrip1.PerformLayout();
 			ResumeLayout(false);
 			PerformLayout();
 		}
@@ -59,5 +96,8 @@
 		#endregion
 
 		private TextBox textBox1;
+		private MenuStrip menuStrip1;
+		private ToolStripMenuItem fileToolStripMenuItem;
+		private ToolStripMenuItem quitToolStripMenuItem;
 	}
 }
